fa556d5822ccf5b14654b7c09d5dfb81a7acc5db
倉本恭丞 <kkz@kuramotoyasushisusumunoMacBook-Air.local>
2026-01-11 13:30:02 +0900

    add cpp04 ex02 template

---
+++ cpp04/ex02/Animal.cpp
@@ -0,0 +1,38 @@
+#include "Animal.hpp"
+
+Animal::Animal() : type("Animal")
+{
+	std::cout << "Animal default constructor called" << std::endl;
+}
+
+Animal::Animal(std::string type) : type(type)
+{
+	std::cout << "Animal type constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal& other) : type(other.type)
+{
+	std::cout << "Animal copy constructor called" << std::endl;
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal destructor called" << std::endl;
+}
+
+Animal& Animal::operator=(const Animal& other)
+{
+	if (this != &other)
+		type = other.type;
+	return *this;
+}
+
+void Animal::makeSound() const
+{
+	std::cout << "Some generic animal sound" << std::endl;
+}
+
+std::string Animal::getType() const
+{
+	return type;
+}
---
+++ cpp04/ex02/Cat.cpp
@@ -0,0 +1,35 @@
+#include "Cat.hpp"
+
+Cat::Cat() : Animal("Cat")
+{
+	std::cout << "Cat default constructor called" << std::endl;
+	brain = new Brain();
+}
+
+Cat::Cat(const Cat& other) : Animal(other)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+	brain = new Brain(*other.brain);
+}
+
+Cat::~Cat()
+{
+	std::cout << "Cat destructor called" << std::endl;
+	delete brain;
+}
+
+Cat& Cat::operator=(const Cat& other)
+{
+	std::cout << "Cat assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		Animal::operator=(other);
+		*brain = *other.brain;
+	}
+	return *this;
+}
+
+void Cat::makeSound() const
+{
+	std::cout << "Meow!" << std::endl;
+}
